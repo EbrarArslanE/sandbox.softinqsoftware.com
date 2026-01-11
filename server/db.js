5ffc6b72c285cc8c7843dad0808f96b89ae9d860
Ebrar Arslan <ebrar.arslan@eronyazilim.com>
2026-01-11 13:22:26 +0300

    v1

--- server/db.js
+++ server/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/sandboxsoftinq");
+    // await mongoose.connect("mongodb://localhost:27017/sandboxsoftinq");
+    await mongoose.connect("mongodb+srv://ebrar_db_user:Ee1313**@cluster-1.yl9a2yx.mongodb.net/ßßß");
     console.log("🍃 DataBase Bağlantısı Başarılı!");
   } catch (err) {
     console.error("❌ DataBase Bağlantısı Başarısız:", err, "!");
