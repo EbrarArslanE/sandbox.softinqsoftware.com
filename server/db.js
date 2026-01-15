9406ce533448ae90cc2430c870eee1c402177c36
Ebrar Arslan <ebrar.arslan@eronyazilim.com>
2026-01-16 01:22:06 +0300

    v1

--- server/db.js
+++ server/db.js
@@ -1,18 +1,18 @@
-import mongoose from "mongoose"
-import ora from "ora"
+import mongoose from "mongoose";
+import ora from "ora";
 
 const connectDB = async () => {
-  const spinner = ora("MongoDB bağlanıyor...").start()
+  const spinner = ora("MongoDB bağlanıyor...").start();
 
   try {
-    await mongoose.connect("mongodb+srv://ebrar_db_user:Ee1313**@cluster-1.yl9a2yx.mongodb.net/ßßß")
+    await mongoose.connect("mongodb+srv://ebrar_db_user:Ee1313**@cluster-1.yl9a2yx.mongodb.net/ßßß");
 
-    spinner.succeed("MongoDB bağlantısı başarılı!")
+    spinner.succeed("MongoDB bağlantısı başarılı!");
   } catch (err) {
-    spinner.fail("MongoDB bağlantısı başarısız!")
-    console.error(err)
-    process.exit(1)
+    spinner.fail("MongoDB bağlantısı başarısız!");
+    console.error(err);
+    process.exit(1);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
